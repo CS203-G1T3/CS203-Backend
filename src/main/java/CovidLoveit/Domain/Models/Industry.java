2c51c3d2597d4a6667c098e985d046f8a8df8887
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-05 15:34:11 +0800

    src/main/java/CovidLoveit/Domain/Models/Industry.java: Created an incomplete Industry Model
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

--- src/main/java/CovidLoveit/Domain/Models/Industry.java
+++ src/main/java/CovidLoveit/Domain/Models/Industry.java
@@ -1,2 +1,28 @@
-package CovidLoveit.Domain.Models;public class Industry {
+package CovidLoveit.Domain.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+public class Industry {
+
+    @Id
+    @GeneratedValue
+    private UUID industryId;
+
+    private String industryName;
+
+    private String industrySubtype;
+
+    private String description;
+
+    // TODO: Create the mappings for industry and guidelines
+    // In this case this will be a one-to-many relationship
+//    private List<Guideline> guidelines = new ArrayList<>();
+
+    // TODO: Generate the getters and setters
 }
