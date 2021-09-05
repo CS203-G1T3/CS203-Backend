7d3849dd7c9975c61d56bd22e8a991888226beab
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-05 15:30:00 +0800

    Removed HelloController sample from Controller package. Removed IndustrySubtype from Domain.Model package. Mapped industry attributes into an entity
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

--- src/main/java/CovidLoveit/Domain/Models/Industry.java
+++ src/main/java/CovidLoveit/Domain/Models/Industry.java
@@ -1,17 +1,2 @@
-package CovidLoveit.Domain.Models;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="INDUSTRY")
-public class Industry {
-
-    @Id
-    private int industryId;
-
-    private String name;
-
-    private String description;
+package CovidLoveit.Domain.Models;public class Industry {
 }
