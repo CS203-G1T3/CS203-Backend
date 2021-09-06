8610d1612d9a59d39d155d1aff829ecbc1d201dd
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-06 23:44:26 +0800

    src/main/java/CovidLoveit/Exception/ResourceNotFoundException.java: Included a custom serial number for the exception class
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

--- src/main/java/CovidLoveit/Exception/ResourceNotFoundException.java
+++ src/main/java/CovidLoveit/Exception/ResourceNotFoundException.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus
 public class ResourceNotFoundException extends RuntimeException{
 
+    private static final long serialVersionUID = 1L;
+
     public ResourceNotFoundException (String message){
         super(message);
     }
