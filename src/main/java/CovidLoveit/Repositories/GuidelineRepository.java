cfd4f382e5155b937458fbf626b74bdf69f3aa0e
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-07 20:23:48 +0800

    src/main/java/CovidLoveit/Repositories/GuidelineRepository.java: Removed unused imports
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

--- src/main/java/CovidLoveit/Repositories/GuidelineRepository.java
+++ src/main/java/CovidLoveit/Repositories/GuidelineRepository.java
@@ -3,7 +3,5 @@ package CovidLoveit.Repositories;
 import CovidLoveit.Domain.Models.Guideline;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface GuidelineRepository extends JpaRepository<Guideline, Integer> {
 }
