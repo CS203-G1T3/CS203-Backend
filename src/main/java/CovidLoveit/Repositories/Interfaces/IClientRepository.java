e8bf71c2f4ccce89ba3d6b414b59bc0935afcafc
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-08 15:53:18 +0800

    Replaced JpaRepository implementation to CudRepository implementation
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

--- src/main/java/CovidLoveit/Repositories/Interfaces/IClientRepository.java
+++ src/main/java/CovidLoveit/Repositories/Interfaces/IClientRepository.java
@@ -1,9 +1,8 @@
 package CovidLoveit.Repositories.Interfaces;
 
 import CovidLoveit.Domain.Models.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.repository.CrudRepository;
 import java.util.UUID;
 
-public interface IClientRepository extends JpaRepository<Client, UUID> {
+public interface IClientRepository extends CrudRepository<Client, UUID> {
 }
--- src/main/java/CovidLoveit/Repositories/Interfaces/IGuidelineRepository.java
+++ src/main/java/CovidLoveit/Repositories/Interfaces/IGuidelineRepository.java
@@ -1,7 +1,7 @@
 package CovidLoveit.Repositories.Interfaces;
 
 import CovidLoveit.Domain.Models.Guideline;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface IGuidelineRepository extends JpaRepository<Guideline, Integer> {
+public interface IGuidelineRepository extends CrudRepository<Guideline, Integer> {
 }
