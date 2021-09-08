6d637515cc766a0ecce0c9108b41bc4bd09e419f
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-08 14:28:50 +0800

    src/main/java/CovidLoveit/Repositories/Interfaces/GuidelineRepository.java: Changed the directory
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

--- src/main/java/CovidLoveit/Repositories/Interfaces/GuidelineRepository.java
+++ src/main/java/CovidLoveit/Repositories/Interfaces/GuidelineRepository.java
@@ -1,4 +1,4 @@
-package CovidLoveit.Repositories;
+package CovidLoveit.Repositories.Interfaces;
 
 import CovidLoveit.Domain.Models.Guideline;
 import org.springframework.data.jpa.repository.JpaRepository;
