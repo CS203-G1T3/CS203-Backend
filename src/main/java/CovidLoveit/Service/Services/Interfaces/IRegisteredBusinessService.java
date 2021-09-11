7c2f314bd920616d3a091b07f2a3ce13c842f0d9
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-11 23:43:47 +0800

    src/main/java/CovidLoveit/Service/Services/Interfaces/IRegisteredBusinessService.java: Changed arguments accepted by delete method
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

--- src/main/java/CovidLoveit/Service/Services/Interfaces/IRegisteredBusinessService.java
+++ src/main/java/CovidLoveit/Service/Services/Interfaces/IRegisteredBusinessService.java
@@ -2,10 +2,12 @@ package CovidLoveit.Service.Services.Interfaces;
 
 import CovidLoveit.Domain.Models.RegisteredBusiness;
 
+import java.util.UUID;
+
 public interface IRegisteredBusinessService {
     RegisteredBusiness addBusiness(String name, String desc);
 
     RegisteredBusiness updateBusiness(RegisteredBusiness registeredBusiness);
 
-    void deleteBusiness(RegisteredBusiness registeredBusiness);
+    void deleteBusiness(UUID businessId);
 }
