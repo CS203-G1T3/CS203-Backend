d57ba50e3e42a16038cd994adc2d33d9aada8097
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-15 22:32:07 +0800

    src/main/java/CovidLoveit/Exception/ClientNotFoundException.java: Created individual model exception class
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

---
+++ src/main/java/CovidLoveit/Exception/ClientNotFoundException.java
@@ -0,0 +1,17 @@
+package CovidLoveit.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class ClientNotFoundException extends RuntimeException{
+
+    public ClientNotFoundException (String message) {
+        super(message);
+    }
+
+    public ClientNotFoundException (String message, Throwable throwable){
+        super(message, throwable);
+    }
+
+}
