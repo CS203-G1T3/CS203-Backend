48a760d8d370166cb4a37d4e869bc4df5891538c
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-15 22:32:44 +0800

    src/main/java/CovidLoveit/Exception/GuidelineNotFoundException.java: Created individual model exception class
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

---
+++ src/main/java/CovidLoveit/Exception/GuidelineNotFoundException.java
@@ -0,0 +1,16 @@
+package CovidLoveit.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class GuidelineNotFoundException extends RuntimeException{
+
+    public GuidelineNotFoundException (String message){
+        super(message);
+    }
+
+    public GuidelineNotFoundException (String message, Throwable throwable){
+        super(message, throwable);
+    }
+}
