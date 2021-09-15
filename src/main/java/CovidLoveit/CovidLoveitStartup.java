361e4274feccdac30c5fe572a3a880e1c32ce60f
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-15 22:59:38 +0800

    src/main/java/CovidLoveit/CovidLoveitStartup.java: Enabled JPA Repository injection on startup
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

--- src/main/java/CovidLoveit/CovidLoveitStartup.java
+++ src/main/java/CovidLoveit/CovidLoveitStartup.java
@@ -3,12 +3,15 @@ package CovidLoveit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@Configuration
+@EnableJpaRepositories(basePackages = "CovidLoveit.Repositories.Interfaces")
 public class CovidLoveitStartup {
 
 	public static void main(String[] args) {
-
 		ConfigurableApplicationContext context = SpringApplication.run(CovidLoveitStartup.class, args);
 	}
 
