1cec7af4c4fceab212a609112a9860cdb656f5f1
linsyhen99 <ruwansadris.2020@smu.edu.sg>
2021-09-16 02:29:38 +0800

    Added reason for the http response code
    Signed-off-by: linsyhen99 <ruwansadris.2020@smu.edu.sg>

--- src/main/java/CovidLoveit/Exception/RegisteredBusinessNotFoundException.java
+++ src/main/java/CovidLoveit/Exception/RegisteredBusinessNotFoundException.java
@@ -3,7 +3,7 @@ package CovidLoveit.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Registered Business Not Found")
 public class RegisteredBusinessNotFoundException extends RuntimeException{
 
     public RegisteredBusinessNotFoundException (String message){
