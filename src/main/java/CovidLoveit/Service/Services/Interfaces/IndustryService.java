a144038ac9d6ba7ed5602afba0ade1b0c13c4d61
yuwei <yuwei.soh.2020@scis.smu.edu.sg>
2021-09-21 04:39:12 +0800

    renamed all instances of description to industryDesc for consistency and clarity

--- src/main/java/CovidLoveit/Service/Services/Interfaces/IndustryService.java
+++ src/main/java/CovidLoveit/Service/Services/Interfaces/IndustryService.java
@@ -3,12 +3,15 @@ package CovidLoveit.Service.Services.Interfaces;
 import CovidLoveit.Domain.Models.Industry;
 
 import java.util.UUID;
+import java.util.Optional;
 
 public interface IndustryService {
 
-    Industry addIndustry(String industryName, String industrySubtype, String description);
+    Industry addIndustry(String industryName, String industrySubtype, String industryDesc);
 
     Industry updateIndustry(UUID industryId, Industry industry);
 
     void deleteIndustry(UUID industryId);
+
+    Optional<Industry> getIndustry(UUID industryId);
 }
