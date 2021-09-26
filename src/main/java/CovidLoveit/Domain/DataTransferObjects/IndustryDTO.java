c893e31c60c1821c1f8f8cf87c003d2c77e874d4
yuwei <yuwei.soh.2020@scis.smu.edu.sg>
2021-09-27 00:24:53 +0800

    updated industry DTO

--- src/main/java/CovidLoveit/Domain/DataTransferObjects/IndustryDTO.java
+++ src/main/java/CovidLoveit/Domain/DataTransferObjects/IndustryDTO.java
@@ -1,10 +1,61 @@
 package CovidLoveit.Domain.DataTransferObjects;
 
+import java.util.UUID;
+import java.util.List;
 
 // Data Transfer Object for http JSON response
 public class IndustryDTO {
     
+    private UUID industryId;
+    private String industryName;
+    private String industrySubtype;
+    private String industryDesc;
+    private List<GuidelineDTO> guidelines;
+    private List<RegisteredBusinessDTO> registeredBusinesses;
 
+    public UUID getIndustryId() {
+        return industryId;
+    }
+
+    public String getIndustryName() {
+        return industryName;
+    }
+
+    public void setIndustryName(String industryName) {
+        this.industryName = industryName;
+    }
+
+    public String getIndustrySubtype() {
+        return industrySubtype;
+    }
+
+    public void setIndustrySubtype(String industrySubtype) {
+        this.industrySubtype = industrySubtype;
+    }
+
+    public String getIndustryDesc() {
+        return industryDesc;
+    }
+
+    public void setIndustryDesc(String industryDesc) {
+        this.industryDesc = industryDesc;
+    }
+
+    public List<GuidelineDTO> getGuidelines() {
+        return guidelines;
+    }
+
+    public void setGuidelines(List<GuidelineDTO> guidelines) {
+        this.guidelines = guidelines;
+    }
+
+    public List<RegisteredBusinessDTO> getRegisteredBusinesses() {
+        return registeredBusinesses;
+    }
+
+    public void setRegisteredBusinesses(List<RegisteredBusinessDTO> registeredBusinesses) {
+        this.registeredBusinesses = registeredBusinesses;
+    }
 }
 
 
