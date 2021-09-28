1781c38983ae1ddb3ae2ab341831b98f7ea3887c
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-09-28 22:16:04 +0800

    src/main/java/CovidLoveit/Repositories/Interfaces/ClientRepository.java: Added getByEmail for clientRepository
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

--- src/main/java/CovidLoveit/Repositories/Interfaces/ClientRepository.java
+++ src/main/java/CovidLoveit/Repositories/Interfaces/ClientRepository.java
@@ -2,7 +2,11 @@ package CovidLoveit.Repositories.Interfaces;
 
 import CovidLoveit.Domain.Models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+
+    Optional<Client> findByEmail(String email);
 }
