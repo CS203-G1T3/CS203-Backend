40308ff4b35b463117a1df6ddbebc1b7d4687bea
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-10-02 17:34:28 +0800

    src/main/java/CovidLoveit/CovidLoveitStartup.java: Added seed data for admin role user and initialised passwordEncryption in the startup file
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

--- src/main/java/CovidLoveit/CovidLoveitStartup.java
+++ src/main/java/CovidLoveit/CovidLoveitStartup.java
@@ -1,26 +1,50 @@
 package CovidLoveit;
 
+import CovidLoveit.Domain.Models.Client;
+import CovidLoveit.Domain.Models.Role;
+import CovidLoveit.Service.Services.Interfaces.ClientService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 @Configuration
 @EnableJpaRepositories(basePackages = "CovidLoveit.Repositories.Interfaces")
 public class CovidLoveitStartup {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(CovidLoveitStartup.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(CovidLoveitStartup.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(ClientService clientService) {
+        return args -> {
+            clientService.addRole(new Role("ADMIN"));
+            clientService.addRole(new Role("USER"));
+
+            clientService.addClient(new Client("ruwan", "ruwan199996", "linsyhen99@gmail.com"));
+
+            clientService.addRoleToClient("ruwan", "ADMIN");
+        };
+    }
 
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
 
-
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
