66272e1c264b4c08d49e2546e0d604709310f3cf
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-10-02 17:49:20 +0800

    src/main/java/CovidLoveit/Utils: Created a utils directory
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

---
+++ src/main/java/CovidLoveit/Utils/Filter/CustomAuthenticationFilter.java
@@ -0,0 +1,80 @@
+package CovidLoveit.Utils.Filter;
+
+import CovidLoveit.Domain.Models.CustomUserDetails;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private Logger logger = LoggerFactory.getLogger(CustomAuthenticationFilter.class);
+    private final AuthenticationManager authenticationManager;
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, password);
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+        CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
+        // TODO: Change the secret key to application.properties file. Figure out how
+        Algorithm algorithm = Algorithm.HMAC256("abcdefghi".getBytes());
+        String access_token = JWT.create()
+                .withSubject(user.getUsername())
+                // Access Token valid time set to 10 minutes
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 10 * 60))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()))
+                .sign(algorithm);
+        String refresh_token = JWT.create()
+                .withSubject(user.getUsername())
+                // Refresh Token valid time set to 15 days
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 15 * 60 * 60 * 24))
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim("roles", user.getAuthorities()
+                        .stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.toList()))
+                .sign(algorithm);
+
+        Map<String, String> tokens = new HashMap<>();
+        tokens.put("access_token", access_token);
+        tokens.put("refresh_token", refresh_token);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+    }
+}
---
+++ src/main/java/CovidLoveit/Utils/Security/SecurityConfiguration.java
@@ -0,0 +1,78 @@
+package CovidLoveit.Utils.Security;
+
+import CovidLoveit.Service.Services.CustomUserDetailsServiceImpl;
+import CovidLoveit.Utils.Filter.CustomAuthenticationFilter;
+import CovidLoveit.Utils.Filter.CustomAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+
+@Configuration @EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailsServiceImpl userDetailsService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public SecurityConfiguration(CustomUserDetailsServiceImpl userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder){
+        this.userDetailsService = userDetailsService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    // TODO: Change the API endpoints to your own custom endpoints
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/login");
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.logout()
+                .permitAll()
+                .and()
+            .authorizeRequests()
+                .antMatchers("/api/login/**", "/api/token/refresh/**").permitAll()
+                .antMatchers(GET, "/**/clients/*").hasRole("ADMIN")
+                .and()
+            .httpBasic();
+
+        http.authorizeRequests()
+                .antMatchers(POST, "/**/guideline/*").hasRole("ADMIN")
+                .antMatchers(PUT, "/**/guideline/*").hasRole("ADMIN")
+                .antMatchers(DELETE, "/**/guideline/*").hasRole("ADMIN")
+                .and()
+            .httpBasic();
+
+        http.authorizeRequests()
+                .antMatchers(POST, "/**/industry/*").hasRole("ADMIN")
+                .antMatchers(PUT, "/**/industry/*").hasRole("ADMIN")
+                .antMatchers(DELETE, "/**/industry/*").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+            .httpBasic();
+        http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
