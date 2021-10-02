a53fb5c9af35c5b490ee43e5ea1a53e268e82139
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-10-02 17:47:38 +0800

    src/main/java/CovidLoveit/Exception/RoleException.java: Created custom RoleException
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

---
+++ src/main/java/CovidLoveit/Exception/RoleException.java
@@ -0,0 +1,15 @@
+package CovidLoveit.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class RoleException extends RuntimeException {
+    public RoleException(String message) {
+        super(message);
+    }
+
+    public RoleException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
