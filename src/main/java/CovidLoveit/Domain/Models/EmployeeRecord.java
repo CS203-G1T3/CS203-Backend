9cbdef04ecc1355f6d9520305d5f4a61e8a41e20
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-10-05 21:25:06 +0800

    src/main/java/CovidLoveit/Domain/Models/EmployeeRecord.java: Implemented EmployeeRecord Entity
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

---
+++ src/main/java/CovidLoveit/Domain/Models/EmployeeRecord.java
@@ -0,0 +1,99 @@
+package CovidLoveit.Domain.Models;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+public class EmployeeRecord {
+
+    public EmployeeRecord() {
+    }
+
+    public EmployeeRecord(String employeeId, String employeeName, LocalDate dateOfBirth,
+                          String vaccine, LocalDate lastSwabDate, String swabResult) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.dateOfBirth = dateOfBirth;
+        this.vaccine = vaccine;
+        this.lastSwabDate = lastSwabDate;
+        this.swabResult = swabResult;
+    }
+
+    @Id
+    @Column(name = "EID", unique = true, nullable = false)
+    private String employeeId;
+
+    @Column(name = "employee_name")
+    private String employeeName;
+
+    @Column(name = "DOB")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "Vaccine")
+    private String vaccine;
+
+    @Column(name = "Last_swab_date")
+    private LocalDate lastSwabDate;
+
+    @Column(name = "swab_result")
+    private String swabResult;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "businessId")
+    private RegisteredBusiness business;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getVaccine() {
+        return vaccine;
+    }
+
+    public void setVaccine(String vaccine) {
+        this.vaccine = vaccine;
+    }
+
+    public LocalDate getLastSwabDate() {
+        return lastSwabDate;
+    }
+
+    public void setLastSwabDate(LocalDate lastSwabDate) {
+        this.lastSwabDate = lastSwabDate;
+    }
+
+    public String getSwabResult() {
+        return swabResult;
+    }
+
+    public void setSwabResult(String swabResult) {
+        this.swabResult = swabResult;
+    }
+
+    public RegisteredBusiness getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(RegisteredBusiness business) {
+        this.business = business;
+    }
+}
