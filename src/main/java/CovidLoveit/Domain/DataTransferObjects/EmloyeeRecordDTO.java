11eb069ac0b094e1f63eb14700b69dca1bf821cd
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-10-05 21:25:41 +0800

    src/main/java/CovidLoveit/Domain/DataTransferObjects/EmloyeeRecordDTO.java: Created employee record DTO
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

---
+++ src/main/java/CovidLoveit/Domain/DataTransferObjects/EmloyeeRecordDTO.java
@@ -0,0 +1,61 @@
+package CovidLoveit.Domain.DataTransferObjects;
+
+import java.time.LocalDate;
+
+public class EmloyeeRecordDTO {
+
+    private String employeeId;
+    private String employeeName;
+    private LocalDate dateOfBirth;
+    private String vaccine;
+    private LocalDate lastSwabDate;
+    private String swabResult;
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getVaccine() {
+        return vaccine;
+    }
+
+    public void setVaccine(String vaccine) {
+        this.vaccine = vaccine;
+    }
+
+    public LocalDate getLastSwabDate() {
+        return lastSwabDate;
+    }
+
+    public void setLastSwabDate(LocalDate lastSwabDate) {
+        this.lastSwabDate = lastSwabDate;
+    }
+
+    public String getSwabResult() {
+        return swabResult;
+    }
+
+    public void setSwabResult(String swabResult) {
+        this.swabResult = swabResult;
+    }
+}
