85d4d90dfe51d7aa6270df4b48c1e99a0d972b04
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-10-05 16:21:48 +0800

    src/main/java/CovidLoveit/Service/Services/IndustryServiceImpl.java: Properly verify if user is admin
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

--- src/main/java/CovidLoveit/Service/Services/IndustryServiceImpl.java
+++ src/main/java/CovidLoveit/Service/Services/IndustryServiceImpl.java
@@ -65,48 +65,68 @@ public class IndustryServiceImpl implements IndustryService {
     @Override
     public Industry updateIndustry(String clientId, IndustryInputModel inputModel) {
         Optional<Industry> industryOptional = industryRepository.findById(inputModel.getIndustryId());
-
-        var sessionUser = clientService.getClient(UUID.fromString(clientId));
-        if (sessionUser == null)
-            throw new ClientException(String.format("Client with ID {%s} not found", clientId));
-
-        if (!sessionUser.getRoles().contains("ROLE_ADMIN"))
-            throw new ClientException("Unauthorized access.");
-
         industryOptional.orElseThrow(() -> {
             logger.warn(String.format("Industry with ID {%s} does not exist in DB.", inputModel.getIndustryId()));
             throw new IndustryException(String.format("Industry with ID {%s} not found.", inputModel.getIndustryId()));
         });
 
-        var industryRecord = industryOptional.get();
-        industryRecord.setIndustryName(inputModel.getIndustryName());
-        industryRecord.setIndustrySubtype(inputModel.getIndustrySubtype());
-        industryRecord.setIndustryDesc(inputModel.getIndustryDesc());
+        var sessionUser = clientService.getClient(UUID.fromString(clientId));
+        if (sessionUser == null)
+            throw new ClientException(String.format("Client with ID {%s} not found", clientId));
 
-        industryRepository.save(industryRecord);
-        logger.info(String.format("Successfully updated industry with ID {%s}", inputModel.getIndustryId()));
-        return industryRecord;
+        boolean isAdmin = false;
+        var roles = sessionUser.getRoles();
+        for(Role role: roles) {
+            if(role.getRoleName().equals("ADMIN")){
+                isAdmin = true;
+                break;
+            }
+        }
 
+        if(isAdmin) {
+            var industryRecord = industryOptional.get();
+            industryRecord.setIndustryName(inputModel.getIndustryName());
+            industryRecord.setIndustrySubtype(inputModel.getIndustrySubtype());
+            industryRecord.setIndustryDesc(inputModel.getIndustryDesc());
+
+            industryRepository.save(industryRecord);
+            logger.info(String.format("Successfully updated industry with ID {%s}", inputModel.getIndustryId()));
+            return industryRecord;
+
+        } else {
+            throw new IndustryException("Unauthorized access.");
+        }
     }
 
     @Override
     public void deleteIndustry(String clientId, UUID industryId) {
         Optional<Industry> industryOptional = industryRepository.findById(industryId);
-
-        var sessionUser = clientService.getClient(UUID.fromString(clientId));
-        if (sessionUser == null)
-            throw new ClientException(String.format("Client with ID {%s} not found", clientId));
-
-        if (!sessionUser.getRoles().contains("ROLE_ADMIN"))
-            throw new ClientException("Unauthorized access.");
-
         industryOptional.orElseThrow(() -> {
             logger.warn(String.format("Industry with ID {%s} does not exist in DB.", industryId));
             throw new IndustryException(String.format("Industry with ID {%s} not found.",industryId));
         });
 
-        industryRepository.delete(industryOptional.get());
-        logger.info(String.format("Successfully removed Industry with ID {%s}", industryId));
+        var sessionUser = clientService.getClient(UUID.fromString(clientId));
+        if (sessionUser == null)
+            throw new ClientException(String.format("Client with ID {%s} not found", clientId));
+
+        boolean isAdmin = false;
+        var roles = sessionUser.getRoles();
+        for(Role role: roles) {
+            if(role.getRoleName().equals("ADMIN")){
+                isAdmin = true;
+                break;
+            }
+        }
+
+        if(isAdmin) {
+            industryRepository.delete(industryOptional.get());
+            logger.info(String.format("Successfully removed Industry with ID {%s}", industryId));
+            return;
+
+        } else {
+            throw new IndustryException("Unauthorized access.");
+        }
     }
 
     @Override
