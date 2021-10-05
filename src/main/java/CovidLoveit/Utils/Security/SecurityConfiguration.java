caa1dd4a348571b21b5f6348518e44df619ab481
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-10-05 13:31:22 +0800

    src/main/java/CovidLoveit/Utils/Security/SecurityConfiguration.java: Allowing CORS on spring security level
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

--- src/main/java/CovidLoveit/Utils/Security/SecurityConfiguration.java
+++ src/main/java/CovidLoveit/Utils/Security/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import CovidLoveit.Utils.Filter.CustomAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
@@ -45,20 +50,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(STATELESS);
 
         // Delete the HTTP-only cookie upon logout
-        http.logout().deleteCookies("remember-me").permitAll();
-
-        http.formLogin();
+        http.cors()
+            .and()
+            .formLogin()
+            .permitAll()
+            .and()
+            .logout()
+            .deleteCookies("remember-me")
+            .permitAll();
 
         http.authorizeRequests()
-                .antMatchers(GET, "/**/clients/*").hasRole("ADMIN")
-                .antMatchers(POST, "/**/guideline/*").hasRole("ADMIN")
-                .antMatchers(PUT, "/**/guideline/*").hasRole("ADMIN")
-                .antMatchers(DELETE, "/**/guideline/*").hasRole("ADMIN")
-                .antMatchers(POST, "/**/industry/*").hasRole("ADMIN")
-                .antMatchers(PUT, "/**/industry/*").hasRole("ADMIN")
-                .antMatchers(DELETE, "/**/industry/*").hasRole("ADMIN")
-                .antMatchers("/api/login/**", "/api/token/refresh/**").permitAll()
-                .anyRequest().authenticated();
+            .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+            .anyRequest().permitAll();
+//                .antMatchers(GET, "/**/clients/*").hasRole("ADMIN")
+//                .antMatchers(POST, "/**/guideline/*").hasRole("ADMIN")
+//                .antMatchers(PUT, "/**/guideline/*").hasRole("ADMIN")
+//                .antMatchers(DELETE, "/**/guideline/*").hasRole("ADMIN")
+//                .antMatchers(POST, "/**/industry/*").hasRole("ADMIN")
+//                .antMatchers(PUT, "/**/industry/*").hasRole("ADMIN")
+//                .antMatchers(DELETE, "/**/industry/*").hasRole("ADMIN")
+//                .antMatchers("/api/login/**", "/api/token/refresh/**").permitAll()
+//                .anyRequest().authenticated();
 
         http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
@@ -68,6 +80,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+//        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
