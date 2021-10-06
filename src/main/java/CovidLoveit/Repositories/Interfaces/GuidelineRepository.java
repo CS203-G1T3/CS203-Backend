7650c1b869d7095c68a43c702ba5299f27634ad3
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-10-06 21:16:51 +0800

    src/main/java/CovidLoveit/Repositories/Interfaces/GuidelineRepository.java: Created custom query to handle getting latest guideine by industry id
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

--- src/main/java/CovidLoveit/Repositories/Interfaces/GuidelineRepository.java
+++ src/main/java/CovidLoveit/Repositories/Interfaces/GuidelineRepository.java
@@ -4,7 +4,13 @@ import CovidLoveit.Domain.Models.Guideline;
 
 import java.util.UUID;
 
+import CovidLoveit.Domain.Models.Industry;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GuidelineRepository extends JpaRepository<Guideline, UUID> {
+
+    @Query("SELECT g1 FROM Guideline g1 where g1.industry = :industry and g1.createdAt=(SELECT max(g2.createdAt) FROM Guideline g2 where g2.industry = :industry)")
+    Guideline findLatestGuidelineByIndustry(@Param("industry") Industry industry);
 }
