7505291f5e6432158c029d8fe2f959acb1172e0f
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-10-06 20:28:27 +0800

    src/main/java/CovidLoveit/Repositories/Interfaces/IndustryRepository.java: Removed faulty query methods
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

--- src/main/java/CovidLoveit/Repositories/Interfaces/IndustryRepository.java
+++ src/main/java/CovidLoveit/Repositories/Interfaces/IndustryRepository.java
@@ -12,11 +12,5 @@ public interface IndustryRepository extends JpaRepository<Industry, UUID> {
     @Query("SELECT DISTINCT industryName FROM Industry")
     List<String> findDistinctIndustryName();
 
-    List<Industry> findByIndustryName(String industryName);
-
-    @Query("SELECT industrySubtype FROM Industry")
-    List<Industry> getIndustrySubtypes();
-
-    @Query("SELECT industrySubtype FROM Industry WHERE industryName = :#{#industry_name}")
-    List<Industry> findIndustrySubtypesByIndustryName(@Param("industry_name") String industryName);
+    List<Industry> findIndustryByIndustryName(String industryName);
 }
