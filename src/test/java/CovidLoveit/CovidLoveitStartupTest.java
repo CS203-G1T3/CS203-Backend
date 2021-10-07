4f7ad4a10a592f3ccff0fd8ddcf08aeed607dee6
Ruwan Sadris <ruwansadris@Mac-N-Cheese.localdomain>
2021-10-08 04:16:49 +0800

    src/test/java/CovidLoveit/CovidLoveitStartupTest.java: Renamed the file according to the main class
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.localdomain>

--- src/test/java/CovidLoveit/CovidLoveitStartupTest.java
+++ src/test/java/CovidLoveit/CovidLoveitStartupTest.java
@@ -3,8 +3,7 @@ package CovidLoveit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DemoApplicationTests {
+class CovidLoveitStartup {
 
 	@Test
 	void contextLoads() {
