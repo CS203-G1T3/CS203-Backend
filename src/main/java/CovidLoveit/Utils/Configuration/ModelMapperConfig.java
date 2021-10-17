93102e3d4d8f6c027df17775ed167474a343dae8
Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>
2021-10-17 15:58:38 +0800

    src/main/java/CovidLoveit/Utils/Configuration/: Created a configuration file for model mapper initialization and password encoder initialization
    Signed-off-by: Ruwan Sadris <ruwansadris@Mac-N-Cheese.local>

---
+++ src/main/java/CovidLoveit/Utils/Configuration/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package CovidLoveit.Utils.Configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
