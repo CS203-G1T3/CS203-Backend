ccb9c2ace18018141fec3cd7e755913b5fb0b763
linsyhen99 <linsyhen99@gmail.com>
2021-10-20 21:42:11 +0800

    src/main/java/CovidLoveit/Exception/GrantException.java: handling exceptions thrown by grants with a customised exception class
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

---
+++ src/main/java/CovidLoveit/Exception/GrantException.java
@@ -0,0 +1,17 @@
+package CovidLoveit.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Grant Not Found")
+public class GrantException extends RuntimeException{
+
+    public GrantException(String message){
+        super(message);
+    }
+
+    public GrantException(String message, Throwable throwable){
+        super(message, throwable);
+    }
+
+}
