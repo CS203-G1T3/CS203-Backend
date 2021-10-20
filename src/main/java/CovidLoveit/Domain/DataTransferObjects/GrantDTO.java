201b5e32b10ec61a5562830cef8ae67ec17194ac
linsyhen99 <linsyhen99@gmail.com>
2021-10-20 21:38:34 +0800

    src/main/java/CovidLoveit/Domain/DataTransferObjects/GrantDTO.java: View model for Grant Entity to limit the return parameters
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

---
+++ src/main/java/CovidLoveit/Domain/DataTransferObjects/GrantDTO.java
@@ -0,0 +1,100 @@
+package CovidLoveit.Domain.DataTransferObjects;
+
+import CovidLoveit.Domain.Models.Industry;
+
+import java.util.List;
+import java.util.UUID;
+
+public class GrantDTO {
+
+    private UUID grantId;
+    private String grantName;
+    private String provider;
+    private String grantDesc;
+    private int value;
+    private String eligibilityCriteria;
+    private String applicationProcess;
+    private String benefits;
+    private String grantLink;
+    private List<Industry> industries;
+
+    public UUID getGrantId() {
+        return grantId;
+    }
+
+    public void setGrantId(UUID grantId) {
+        this.grantId = grantId;
+    }
+
+    public String getGrantName() {
+        return grantName;
+    }
+
+    public void setGrantName(String grantName) {
+        this.grantName = grantName;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public String getGrantDesc() {
+        return grantDesc;
+    }
+
+    public void setGrantDesc(String grantDesc) {
+        this.grantDesc = grantDesc;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getEligibilityCriteria() {
+        return eligibilityCriteria;
+    }
+
+    public void setEligibilityCriteria(String eligibilityCriteria) {
+        this.eligibilityCriteria = eligibilityCriteria;
+    }
+
+    public String getApplicationProcess() {
+        return applicationProcess;
+    }
+
+    public void setApplicationProcess(String applicationProcess) {
+        this.applicationProcess = applicationProcess;
+    }
+
+    public String getBenefits() {
+        return benefits;
+    }
+
+    public void setBenefits(String benefits) {
+        this.benefits = benefits;
+    }
+
+    public String getGrantLink() {
+        return grantLink;
+    }
+
+    public void setGrantLink(String grantLink) {
+        this.grantLink = grantLink;
+    }
+
+    public List<Industry> getIndustries() {
+        return industries;
+    }
+
+    public void setIndustries(List<Industry> industries) {
+        this.industries = industries;
+    }
+}
