7868cb80e5e7b1ed9556e266d10068b0be89fcfc
linsyhen99 <linsyhen99@gmail.com>
2021-10-20 21:42:50 +0800

    src/main/java/CovidLoveit/Repositories/Interfaces/GrantRepository.java: Database access layer for grants
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

---
+++ src/main/java/CovidLoveit/Repositories/Interfaces/GrantRepository.java
@@ -0,0 +1,13 @@
+package CovidLoveit.Repositories.Interfaces;
+
+import CovidLoveit.Domain.Models.Grant;
+import CovidLoveit.Domain.Models.Industry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface GrantRepository extends JpaRepository<Grant, UUID> {
+
+    List<Grant> findGrantsByIndustries(Industry industry);
+}
