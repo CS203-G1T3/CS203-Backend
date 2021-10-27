7ad6f749868a0cface81dc9e337121e5668b9c0a
linsyhen99 <linsyhen99@gmail.com>
2021-10-27 15:06:46 +0800

    src/main/java/CovidLoveit/Repositories/Interfaces/GrantRepository.java: Added a new method to retrieve grant by grant name
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- src/main/java/CovidLoveit/Repositories/Interfaces/GrantRepository.java
+++ src/main/java/CovidLoveit/Repositories/Interfaces/GrantRepository.java
@@ -5,9 +5,12 @@ import CovidLoveit.Domain.Models.Industry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface GrantRepository extends JpaRepository<Grant, UUID> {
 
     List<Grant> findGrantsByIndustries(Industry industry);
+
+    Optional<Grant> findGrantByGrantName(String grantName);
 }
