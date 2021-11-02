834a35963bbeb614ed330d5e8a866c5ccd5254a0
linsyhen99 <linsyhen99@gmail.com>
2021-11-02 22:04:45 +0800

    Removed hard coded error message for the exception thrown
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- src/main/java/CovidLoveit/Exception/GuidelineException.java
+++ src/main/java/CovidLoveit/Exception/GuidelineException.java
@@ -3,7 +3,7 @@ package CovidLoveit.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Guideline Not Found")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class GuidelineException extends RuntimeException{
 
     public GuidelineException(String message){
