41d164c7484b744b338e5f5f3736601967f23ba1
linsyhen99 <linsyhen99@gmail.com>
2021-11-04 17:46:59 +0800

    src/main/java/CovidLoveit/Utils/Security/SecurityConfiguration.java: Included permissions to access health check endpoint
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- src/main/java/CovidLoveit/Utils/Security/SecurityConfiguration.java
+++ src/main/java/CovidLoveit/Utils/Security/SecurityConfiguration.java
@@ -53,7 +53,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/**/client").permitAll()
             .antMatchers("/**/role").hasRole("ADMIN")
             .antMatchers("/**/guideline/*").permitAll()
-            .antMatchers("/api/v1/healthcheck").permitAll()
+            .antMatchers("/api/health-check/**").permitAll()
             .anyRequest().authenticated().and()
             .sessionManagement().sessionCreationPolicy(STATELESS).and()
             .addFilter(customAuthenticationFilter)
