831ffdc5ce093291227958f4da57e9298ed7f6c9
linsyhen99 <linsyhen99@gmail.com>
2021-11-04 17:47:22 +0800

    src/main/java/CovidLoveit/Controllers/Authentication/AuthController.java: Renamed the healthcheck endpoint
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- src/main/java/CovidLoveit/Controllers/Authentication/AuthController.java
+++ src/main/java/CovidLoveit/Controllers/Authentication/AuthController.java
@@ -9,9 +9,12 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,7 +34,7 @@ import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 public class AuthController {
 
     private Logger logger = LoggerFactory.getLogger(AuthController.class);
@@ -87,7 +90,7 @@ public class AuthController {
     }
 
     // for load balancer
-    @GetMapping("/healthcheck")
+    @GetMapping("/health-check")
     public Map<String, String> healthCheck() {
         return Collections.singletonMap("response", "all good!");
     }
