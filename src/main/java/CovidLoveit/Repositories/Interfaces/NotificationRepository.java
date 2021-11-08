45f774770074ded56c94dff1e0ebfa8258308d17
linsyhen99 <linsyhen99@gmail.com>
2021-11-08 20:16:26 +0800

    src/main/java/CovidLoveit/Repositories/Interfaces/NotificationRepository.java: Fixed sql query logic to look up the proper columns
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- src/main/java/CovidLoveit/Repositories/Interfaces/NotificationRepository.java
+++ src/main/java/CovidLoveit/Repositories/Interfaces/NotificationRepository.java
@@ -1,5 +1,6 @@
 package CovidLoveit.Repositories.Interfaces;
 
+import CovidLoveit.Domain.Models.Client;
 import CovidLoveit.Domain.Models.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,8 +10,8 @@ import java.util.List;
 import java.util.UUID;
 
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
-    List<Notification> findAllByClientId(UUID clientId);
+    List<Notification> findAllByClient(Client client);
 
-    @Query(value = "SELECT * FROM Notification WHERE clientId = :clientId", nativeQuery = true)
-    List<Notification> findAllUnacknowledgedNotificationByClientId(@Param("clientId") UUID clientId);
+    @Query(value = "SELECT * FROM Notification WHERE client_Id = :clientId and is_Acknowledged = false", nativeQuery = true)
+    List<Notification> findAllUnacknowledgedNotificationByClient(@Param("clientId") UUID clientId);
 }
