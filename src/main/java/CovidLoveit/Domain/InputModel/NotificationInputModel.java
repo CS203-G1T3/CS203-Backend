0e38794b81eaedb753b8b9b4cb686d79b2693582
linsyhen99 <linsyhen99@gmail.com>
2021-11-08 13:42:08 +0800

    src/main/java/CovidLoveit/Domain/InputModel/NotificationInputModel.java: Supported with required constructors
    Signed-off-by: linsyhen99 <linsyhen99@gmail.com>

--- src/main/java/CovidLoveit/Domain/InputModel/NotificationInputModel.java
+++ src/main/java/CovidLoveit/Domain/InputModel/NotificationInputModel.java
@@ -8,16 +8,27 @@ import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 import javax.validation.constraints.NotEmpty;
 import java.util.Set;
+import java.util.UUID;
 
 public class NotificationInputModel {
 
     public NotificationInputModel() {
     }
 
-    public NotificationInputModel(String messageBody, boolean isAcknowledged) {
+    public NotificationInputModel(String messageBody, boolean isAcknowledged, UUID clientId) {
         this.messageBody = messageBody;
         this.isAcknowledged = isAcknowledged;
+        this.clientId = clientId;
     }
+
+    public NotificationInputModel(UUID notifId, String messageBody, boolean isAcknowledged, UUID clientId) {
+        this.notifId = notifId;
+        this.messageBody = messageBody;
+        this.isAcknowledged = isAcknowledged;
+        this.clientId = clientId;
+    }
+
+    private UUID notifId;
 
     @NotEmpty(message = "Please include a notification message body")
     private String messageBody;
@@ -25,10 +36,28 @@ public class NotificationInputModel {
     @JsonProperty(value = "isAcknowledged")
     private boolean isAcknowledged;
 
+    private UUID clientId;
+
     public Set<ConstraintViolation<NotificationInputModel>> validate() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         Validator validator = factory.getValidator();
         return validator.validate(this);
+    }
+
+    public UUID getNotifId() {
+        return notifId;
+    }
+
+    public void setNotifId(UUID notifId) {
+        this.notifId = notifId;
+    }
+
+    public UUID getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(UUID clientId) {
+        this.clientId = clientId;
     }
 
     public String getMessageBody() {
